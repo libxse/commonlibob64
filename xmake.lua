f1173ef6bb5da7928b04d34fd471db91a5a1bc15
Qudix <17361645+Qudix@users.noreply.github.com>
2026-02-14 11:24:49 -0600

    build: move most rule logic to shared

--- xmake.lua
+++ xmake.lua
@@ -12,7 +12,6 @@ add_rules("mode.debug", "mode.releasedbg")
 
 -- include subprojects
 includes("lib/commonlib-shared")
-includes("xmake-rules.lua")
 
 -- define targets
 target("commonlibob64", function ()
@@ -47,4 +46,60 @@ target("commonlibob64", function ()
         "clang_cl::-Wno-undefined-bool-conversion",
         { public = true }
     )
+end)
+
+rule("commonlibob64.plugin", function()
+    add_deps("commonlib.plugin")
+
+    on_load(function(target)
+        target:data_set("commonlib.plugin.config", target:extraconf("rules", "commonlibob64.plugin"))
+    end)
+
+    on_config(function(target)
+        target:add("deps", "commonlibob64")
+
+        target:add("configfiles", path.join(os.scriptdir(), "res/commonlibob64-plugin.cpp.in"))
+        target:add("files", path.join(target:configdir(), "commonlibob64-plugin.cpp"))
+
+        local conf = target:extraconf("rules", "commonlibob64.plugin")
+        if conf.options then
+            if conf.options.sig_scanning then
+                conf.options.address_library = false
+            else
+                conf.options.sig_scanning = false
+                if conf.options.address_library == nil then
+                    conf.options.address_library = true
+                end
+            end
+            if conf.options.no_struct_use then
+                conf.options.layout_dependent = false
+            else
+                conf.options.no_struct_use = false
+                if conf.options.layout_dependent == nil then
+                    conf.options.layout_dependent = true
+                end
+            end
+        else
+            conf.options = {
+                sig_scanning = false,
+                address_library = true,
+                no_struct_use = false,
+                layout_dependent = true
+            }
+        end
+
+        target:set("configvar", "COMMONLIBOB64_OPTION_SIG_SCANNING", tostring(conf.options.sig_scanning))
+        target:set("configvar", "COMMONLIBOB64_OPTION_ADDRESS_LIBRARY", tostring(conf.options.address_library))
+        target:set("configvar", "COMMONLIBOB64_OPTION_NO_STRUCT_USE", tostring(conf.options.no_struct_use))
+        target:set("configvar", "COMMONLIBOB64_OPTION_LAYOUT_DEPENDENT", tostring(conf.options.layout_dependent))
+
+        if os.getenv("XSE_TES4_MODS_PATH") then
+            target:set("installdir", path.join(os.getenv("XSE_TES4_MODS_PATH"), target:name()))
+        elseif os.getenv("XSE_TES4_GAME_PATH") then
+            target:set("installdir", os.getenv("XSE_TES4_GAME_PATH"))
+        end
+
+        target:add("installfiles", target:targetfile(), { prefixdir = "OblivionRemastered/Binaries/Win64/OBSE/Plugins" })
+        target:add("installfiles", target:symbolfile(), { prefixdir = "OblivionRemastered/Binaries/Win64/OBSE/Plugins" })
+    end)
 end)
